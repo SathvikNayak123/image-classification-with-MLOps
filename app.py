c02de252f48df4d23f7ad379debb8f973b7a2f8e
SathvikNayak123 <sathviknayak2@gmail.com>
2024-08-12 17:48:31 +0530

    app_done

--- app.py
+++ app.py
@@ -1,11 +1,13 @@
 from flask import Flask, request, jsonify, render_template
 import os
 from flask_cors import CORS, cross_origin
+from cnnClassifier.pipeline.predict import PredictionPipeline
 from cnnClassifier.utils.common import decodeImage
-from cnnClassifier.pipeline.prediction import PredictPipeline
 
-os.putenv('LANG', 'en_US.UTF-8')
-os.putenv('LC_ALL', 'en_US.UTF-8')
+
+
+os.environ['LANG'] = 'en_US.UTF-8'
+os.environ['LC_ALL'] = 'en_US.UTF-8'
 
 app = Flask(__name__)
 CORS(app)
@@ -15,16 +17,16 @@ class ClientApp:
         self.filename = "inputImage.jpg"
         self.classifier = PredictionPipeline(self.filename)
 
+clApp = ClientApp()
 
 @app.route("/", methods=['GET'])
 @cross_origin()
 def home():
     return render_template('index.html')
 
-@app.route("/train", methods=['GET','POST'])
+@app.route("/train", methods=['GET', 'POST'])
 @cross_origin()
 def trainRoute():
-    #os.system("python main.py")
     os.system("dvc repro")
     return "Training done successfully!"
 
@@ -37,7 +39,5 @@ def predictRoute():
     return jsonify(result)
 
 
-
 if __name__ == "__main__":
-    clApp = ClientApp()
-    app.run(host='0.0.0.0', port=8080) #for AWS
+    app.run(host='0.0.0.0', port=8080)
